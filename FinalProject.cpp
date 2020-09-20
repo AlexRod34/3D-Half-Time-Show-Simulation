82e3df842feca446708d8bb4e9ee21178f49a94d
AlexRod34 <62714841+AlexRod34@users.noreply.github.com>
2020-09-20 11:11:22 -0400

    Changed rcvbuffer, deleted functions not used, compressed drawUAVs()

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -1,13 +1,17 @@
 /*
 Author: Alex Rodriguez
 Class: ECE4122  
-Last Date Modified: 12/3/2019
+Last Date Modified: 09/19/2020
 Description: This program develops a halftime show using UAVs for the final project.
 Uses openGL and MPI to demo the show.
 Program simulates UAVs flying up to a sphere and revolving around the sphere for ~60 seconds.
 Used Red UAVs since my last name starts with R(our choice of color)
 */
-
+#ifdef __APPLE_CC__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,7 +20,7 @@ Used Red UAVs since my last name starts with R(our choice of color)
 #include <cmath>
 #include <math.h>
 #include <cstdlib>
-#include <GL/glut.h>
+//#include <GLUT/glut.h>
 #include <chrono>
 #include <thread>
 #include "ECE_Bitmap.h"
@@ -28,7 +32,8 @@ int onSurface = 0; // flag to check if on surface
 
 const int numElements = 6; // for x, y, z, vx, vy, vz
 const int rcvSize = 16 * 6; // (Main task + 15 UAVs) * numElements
-double* rcvbuffer = new double[rcvSize]; // receive buffer
+//double* rcvbuffer = new double[rcvSize]; // receive buffer
+double rcvbuffer[rcvSize]; // receive buffer on stack instead of heap in this case of known processes always being 16
 double sendBuffer[numElements]; // send buffer
 
 int volatile flag = 0; // check if positions have been drawn at first
@@ -46,55 +51,13 @@ double fY; // force uav y direction
 double uavForceZ; // force z for a UAV
 double fZ; // force uav z direction with gravity included
 
-double initPosUAVs[45]; // intial positions of each UAV
+// initializes positions for the first time
+double initPosUAVs[45] = {0.0,-45.72,0.0, 0.0,45.72,0.0,23.46,-45.72,0.0,23.46,45.72,0.0,-23.46,-45.72,0.0,0.0,22.86,0.0,-23.46,22.86,0.0,23.46,22.86,0.0,
+    -23.46,-22.86,0.0,23.46,-22.86,0.0,-23.46,0.0,0.0,23.46,0.0,0.0,0.0,0.0,0.0,0.0,-22.86,0.0,-23.46,45.72,0.0};
 
-GLuint texture[2]; // will hold the texture for bitmap
-
-// texture image for size and data
-struct Image {
-
-    unsigned long sizeX;
-
-    unsigned long sizeY;
-
-    char *data;
-
-};
-
-typedef struct Image Image;
+GLuint texture[1]; // will hold the texture for bitmap
 
 BMP inBitmap; // bitmap
-
-
-#define checkImageWidth 64
-
-#define checkImageHeight 64
-
-
-GLubyte checkImage[checkImageWidth][checkImageHeight][3];
-
-// checks image for texture
-void makeCheckImage(void) {
-
-    int i, j, c;
-
-    for (i = 0; i < checkImageWidth; i++) {
-
-        for (j = 0; j < checkImageHeight; j++) {
-
-            c = ((((i & 0x8) == 0) ^ ((j & 0x8) == 0))) * 255;
-
-            checkImage[i][j][0] = (GLubyte)c;
-
-            checkImage[i][j][1] = (GLubyte)c;
-
-            checkImage[i][j][2] = (GLubyte)c;
-
-        }
-
-    }
-
-}
 
 
 //----------------------------------------------------------------------
@@ -116,6 +79,21 @@ void changeSize(int w, int h)
     glViewport(0, 0, w, h); // set viewport (drawing area) to entire window
 }
 
+void processKey(unsigned char key, int x, int y)
+{
+	switch(key) 
+	{
+		case 'q':
+		std::cout << "Done from rank: 0" << std::endl;
+		//free(rcvbuffer);
+		MPI_Abort(MPI_COMM_WORLD,0);
+		//MPI_Finalize();
+		//glutLeaveMainLoop();
+		//exit(0);
+		break;
+	}
+}
+
 
 /*
 Function takes in no input and returns nothing
@@ -128,7 +106,6 @@ void displayFootballField()
     // field is 53 1/3 yards wide, 46.936152m -> /2 = 23.468076
     // x axis is width of field, y axis is length of field
     //3.0 added to adjust for bmp bounds in image
-    glPushMatrix();
     glBindTexture(GL_TEXTURE_2D, texture[0]);
     glBegin(GL_QUADS);
     glTexCoord2f(0, 0);
@@ -140,8 +117,7 @@ void displayFootballField()
     glTexCoord2f(1, 0);
     glVertex2f(-(23.468+3.0), -(54.864+3.0));
     glEnd();
-    glPopMatrix();
-
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 /*
 Function takes in no inputs and returns nothing
@@ -150,204 +126,27 @@ Updates each position based on the receive buffer after the uavs take off
 */
 void drawUAVs()
 {
-    glBindTexture(GL_TEXTURE_2D,0);
     //y coord: 54.864 - 18.288 = 36.576 -> 0 yard line
     //x coord: 23.468076 -> 0 yard line
     glColor3f(1.0, 0.0, 0.0); 
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[0], initPosUAVs[1], initPosUAVs[2]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[6], rcvbuffer[7], rcvbuffer[8]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
+    for(int i = 0; i < 15; i++)
+    {
+    	glPushMatrix();
+    		if(count == 0) // if uav hasnt taken off
+    		{
+    			glTranslatef(initPosUAVs[i*3], initPosUAVs[i*3+1], initPosUAVs[i*3+2]); // i =0 [0,1,2]// i =1 [3,4,5]//i=2 [6,7,8] // i=3 [9,10,11]// i=14 =[42,43,44]
+    			glutSolidSphere(1, 20, 20); //1m sphere
 
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[3], initPosUAVs[4], initPosUAVs[5]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[12], rcvbuffer[13], rcvbuffer[14]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
+    		}
+    		else // draw in new position
+    		{
+    			glTranslatef(rcvbuffer[i*6+6], rcvbuffer[i*6+7], rcvbuffer[i*6+8]); //i=0 [6,7,8] // i =1 [12,13,14] i=2 [18,19,20]
+            	glutSolidSphere(1, 20, 20); //1m sphere
+    		}
 
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[6], initPosUAVs[7], initPosUAVs[8]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[18], rcvbuffer[19], rcvbuffer[20]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
+    	glPopMatrix();
 
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[9], initPosUAVs[10], initPosUAVs[11]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[24], rcvbuffer[25], rcvbuffer[26]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
-
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[12], initPosUAVs[13], initPosUAVs[14]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[30], rcvbuffer[31], rcvbuffer[32]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
-
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[15], initPosUAVs[16], initPosUAVs[17]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[36], rcvbuffer[37], rcvbuffer[38]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
-
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[18], initPosUAVs[19], initPosUAVs[20]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[42], rcvbuffer[43], rcvbuffer[44]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
-
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[21], initPosUAVs[22], initPosUAVs[23]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[48], rcvbuffer[49], rcvbuffer[50]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
-
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[24], initPosUAVs[25], initPosUAVs[26]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[54], rcvbuffer[55], rcvbuffer[56]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
-
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[27], initPosUAVs[28], initPosUAVs[29]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[60], rcvbuffer[61], rcvbuffer[62]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
-
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[30], initPosUAVs[31], initPosUAVs[32]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[66], rcvbuffer[67], rcvbuffer[68]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
-
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[33], initPosUAVs[34], initPosUAVs[35]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[72], rcvbuffer[73], rcvbuffer[74]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
-
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[36], initPosUAVs[37], initPosUAVs[38]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[78], rcvbuffer[79], rcvbuffer[80]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
-
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[39], initPosUAVs[40], initPosUAVs[41]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[84], rcvbuffer[85], rcvbuffer[86]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
-
-    glPushMatrix();
-        if(count == 0) // if uav hasnt taken off
-        {
-            glTranslatef(initPosUAVs[42], initPosUAVs[43], initPosUAVs[44]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-        else // draw in new position
-        {
-            glTranslatef(rcvbuffer[90], rcvbuffer[91], rcvbuffer[92]);
-            glutSolidSphere(1, 20, 20); //1m sphere
-        }
-    glPopMatrix();
+    }
 
     count++; // done with initial positions
 }
@@ -388,13 +187,13 @@ void renderScene()
     glPushMatrix();
         glColor3f(0.0, 0.0, 1.0);
         glTranslatef(0, 0, 50.0);
-        glutWireSphere(10, 10, 10); // wire sphere centered at 0,0,50 with 10m radius
+        glutWireSphere(9.54, 10, 10); // wire sphere centered at 0,0,50 with 9.54m radius
     glPopMatrix();
 
     glutSwapBuffers(); // Make it all visible
     
     // gathers every rank info
-    MPI_Allgather(sendBuffer, numElements, MPI_DOUBLE, rcvbuffer, numElements, MPI_DOUBLE, MPI_COMM_WORLD);
+    MPI_Allgather(sendBuffer, numElements, MPI_DOUBLE, &rcvbuffer, numElements, MPI_DOUBLE, MPI_COMM_WORLD);
 }
 
 //----------------------------------------------------------------------
@@ -431,15 +230,13 @@ void mainOpenGL(int argc, char**argv)
 
     inBitmap.read("ff.bmp"); // reads in bitmap of football field
 
-    makeCheckImage();
-
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
     // Create Textures
 
-    glGenTextures(2, texture);
+    glGenTextures(1, texture);
     
-    // Setup first texture
+    // Setup texture
     glBindTexture(GL_TEXTURE_2D, texture[0]);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); //scale linearly when image bigger than texture
@@ -450,29 +247,13 @@ void mainOpenGL(int argc, char**argv)
     glTexImage2D(GL_TEXTURE_2D, 0, 3, inBitmap.bmp_info_header.width, inBitmap.bmp_info_header.height, 0,
         GL_BGR_EXT, GL_UNSIGNED_BYTE, &inBitmap.data[0]);
 
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-
-    //Do the second texture
-
-    glBindTexture(GL_TEXTURE_2D, texture[1]);
-
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, checkImageWidth, checkImageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, &checkImage[0][0][0]);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL); // sets up environment of texture "color", no blending or anything like that
 
     glEnable(GL_TEXTURE_2D);
 
-
-    glutReshapeFunc(changeSize); // changes size of window in case user expands window
     glutDisplayFunc(renderScene); // renders all of scene
+    glutReshapeFunc(changeSize); // changes size of window in case user expands window
+    glutKeyboardFunc(processKey);
     glutTimerFunc(100, timerFunction, 0); // 100ms refresh rate called
     glutMainLoop();
 }
@@ -514,7 +295,7 @@ void CalcualteUAVsLocation(int rank)
         double normDirX = dirX/magnitudeXYZ;
         double normDirY = dirY/magnitudeXYZ;
         double normDirZ = dirZ/magnitudeXYZ;
-        double uavForceX = 0.5; // 5N force
+        uavForceX = 0.3; // 5N force
         fX = 0.5; //(uavForceX*rank); ((double)rank)/3.0 // force in x direction
         fY = (8.0-(double)rank/2); // force in Y direction
         uavForceZ = 9.4; // base force needed to counter gravity
@@ -560,7 +341,7 @@ void CalcualteUAVsLocation(int rank)
         sendBuffer[4] = newVelY; // new vy position
         sendBuffer[5] = newVelZ; // new vz position
     }
-    else if(distP1P2 <= 11.0) // checks if close to sphere and ready to revolve around it
+    else if(distP1P2 <= 12.0) // checks if close to sphere and ready to revolve around it
     {
         onSurface = 1; // is now on surface
         if(deltaU > 61) // timestep reset
@@ -571,12 +352,12 @@ void CalcualteUAVsLocation(int rank)
         double x = 0; 
         double y = 0;
         double z = 0;
-        double r = 1;
+        double r = 1.0;//1.0;
 
         // parametric equations for calculating new positions every timestep around a sphere surface
-        x = -r*sin(2*PI*(u*deltaU));
-        y = r*cos(2*PI*(u*deltaU));
-        z = 0;
+        x = -r*sin(2*PI*(u*deltaU)); // changed from sin
+        y = r*cos(2*PI*(u*deltaU)); // changed from cos
+        z = 0; // changed from 0
 
         // new direction vector
         double dirX = 0 - x;
@@ -598,9 +379,9 @@ void CalcualteUAVsLocation(int rank)
         y = yPrime;
         z = zPrime;
         ++deltaU; // increased time step for next iteration
-        sendBuffer[0] = x + rcvbuffer[rank*5+rank]; // new x position
-        sendBuffer[1] = y + rcvbuffer[rank*5+rank+1]; // new y position
-        sendBuffer[2] = z + rcvbuffer[rank*5+rank+2]; // new z position
+        sendBuffer[0] = x+ rcvbuffer[rank*5+rank]; // new x position
+        sendBuffer[1] = y+ rcvbuffer[rank*5+rank+1]; // new y position
+        sendBuffer[2] = z+ rcvbuffer[rank*5+rank+2]; // new z position
     }
 
 }
@@ -616,53 +397,6 @@ void CalcualteUAVsLocation(int rank)
 int main(int argc, char**argv)
 
 {
-    // initializes positions for the first time
-    initPosUAVs[0] = 0.0;
-    initPosUAVs[1] = -45.72;
-    initPosUAVs[2] = 0.0;
-    initPosUAVs[3] = 0.0;
-    initPosUAVs[4] = 45.72;
-    initPosUAVs[5] = 0.0;
-    initPosUAVs[6] = 23.46;
-    initPosUAVs[7] = -45.72;
-    initPosUAVs[8] = 0.0;
-    initPosUAVs[9] = 23.46;
-    initPosUAVs[10] = 45.72;
-    initPosUAVs[11] = 0.0;
-    initPosUAVs[12] = -23.46;
-    initPosUAVs[13] = -45.72;
-    initPosUAVs[14] = 0.0;
-    initPosUAVs[15] = 0.0;
-    initPosUAVs[16] = 22.86;
-    initPosUAVs[17] = 0.0;
-    initPosUAVs[18] = -23.46;
-    initPosUAVs[19] = 22.86;
-    initPosUAVs[20] = 0.0;
-    initPosUAVs[21] = 23.46;
-    initPosUAVs[22] = 22.86;
-    initPosUAVs[23] = 0.0;
-    initPosUAVs[24] = -23.46;
-    initPosUAVs[25] = -22.86;
-    initPosUAVs[26] = 0.0;
-    initPosUAVs[27] = 23.46; //
-    initPosUAVs[28] = -22.86;
-    initPosUAVs[29] = 0.0;
-    initPosUAVs[30] = -23.46;
-    initPosUAVs[31] = 0.0;
-    initPosUAVs[32] = 0.0;
-    initPosUAVs[33] = 23.46;
-    initPosUAVs[34] = 0.0;
-    initPosUAVs[35] = 0.0;
-    initPosUAVs[36] = 0.0;
-    initPosUAVs[37] = 0.0;
-    initPosUAVs[38] = 0.0;
-    initPosUAVs[39] = 0.0;
-    initPosUAVs[40] = -22.86;
-    initPosUAVs[41] = 0.0;
-    initPosUAVs[42] = -23.46;
-    initPosUAVs[43] = 45.72;
-    initPosUAVs[44] = 0.0;
-
 
     int numTasks;
     int rank;
@@ -686,6 +420,8 @@ int main(int argc, char**argv)
     if (rank == 0) 
     {
         mainOpenGL(argc, argv);
+        std::cout << "got out of mainloop!" << std::endl;
+    	//MPI_Finalize();
     }
     else
     {
@@ -693,12 +429,20 @@ int main(int argc, char**argv)
         std::this_thread::sleep_for(std::chrono::seconds(5));
         for (int ii = 0; ii < 600 ; ii++) // runs for about 68 seconds
         {
+        	// Sleep for 5 seconds
             ++flag;
             CalcualteUAVsLocation(rank); // each rank handles a uav
-            MPI_Allgather(sendBuffer, numElements, MPI_DOUBLE, rcvbuffer, numElements, MPI_DOUBLE, MPI_COMM_WORLD);
+            MPI_Allgather(sendBuffer, numElements, MPI_DOUBLE, &rcvbuffer, numElements, MPI_DOUBLE, MPI_COMM_WORLD);
+        }
+        std::cout << "Done from rank:  " << rank << std::endl;
+        {
+        	// free(rcvbuffer);
+        	// rcvbuffer = NULL;
+        	// std::cout << "Freed from rank: " << rank << std::endl;
+        	MPI_Abort(MPI_COMM_WORLD,1);
+
         }
     }
 
-    //MPI_Finalize();
     return 0;
 }
